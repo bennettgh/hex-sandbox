82c1e88ae84512f573f4c58c030d9ebafac2e387
Andrew Bennett <bennetaw@mcmaster.ca>
2019-11-27 00:08:37 -0500

    .

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+const point = (x,y) => {
+
+}
+
+const GridRound = new Grid({
+  orientation: 'pointy',
+  radius: '3'
+});
+
+const GridRect = new Grid({
+  orientation: 'pointy',
+  width: 10,
+  height: 10,
+});
+
